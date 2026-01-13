40fd679b3fe41446540b78f640aae83b026da6bc
Danpl <styoda6@gmail.com>
2026-01-14 01:00:16 +0300

    new way to print message

---
+++ game_over_v2/game_over_v2.cpp
@@ -0,0 +1,15 @@
+// Game_over_v2
+
+#include <iostream>
+
+/*
+* Директива using дает непосредственный доступ к элементам пространства имен std
+* С помощью этого можно не набирать префикс std::
+*/
+
+using namespace std;
+int main()
+{
+    cout << "Game overr!" << endl;
+    return 0;
+}
