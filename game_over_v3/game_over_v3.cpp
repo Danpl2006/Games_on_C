ab308fe1e8894bda6e78ca4b0cdd9298dd2624f7
Danpl <styoda6@gmail.com>
2026-01-14 01:09:12 +0300

    another way

---
+++ game_over_v3/game_over_v3.cpp
@@ -0,0 +1,16 @@
+// Game_over_v3
+
+#include <iostream>
+
+/*
+* Непосредственное объявление элементов из пространства имен std с которыми будет вестить работа
+*/
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
