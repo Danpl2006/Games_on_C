e6615ba262da0264215ccdd9b26af548bb456197
Danpl <styoda6@gmail.com>
2026-01-17 22:29:42 +0300

    New game_stats

---
+++ game_stats_v2/game_stats_v2.cpp
@@ -0,0 +1,51 @@
+// game_stats_v2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned int score = 5000;
+	cout << "score: " << score << endl;
+
+	// Изменение значения переменной
+	
+	score = score + 100;
+	cout << "score: " << score << endl;
+
+	// Комбинированный оператор присваивания
+
+	score += 100;
+	cout << "score: " << score << endl;
+	
+	// Использование оператора инкремента ++ увеличивающий значение переменной на единицу
+	// ++lives - сначала переменная увеличивается на 1 потом все остальное
+	// lives++ - присвается сначала потом увеличивается на 1
+	// -- декремент, тоже самое только уменьшает на 1
+
+	int lives = 3;
+	++lives;
+	cout << "lives: " << lives << endl;
+
+	lives = 3;
+	lives++;
+	cout << "lives: " << lives << endl;
+
+	lives = 3;
+	int bonus = ++lives * 10;
+	cout << "lives, bonus: " << lives << ', ' << bonus << endl;
+
+	lives = 3;
+	bonus = lives++ * 10;
+	cout << "lives, bonus: " << lives << ', ' << bonus << endl;
+
+	// Целочисленное переполнение
+	score = 4294967295;
+	cout << "\nscore: " << score << endl;
+	++score;
+	cout << "score: " << score << endl;
+	return 0;
+	
+}
+
