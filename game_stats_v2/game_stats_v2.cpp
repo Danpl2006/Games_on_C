55607d761f674a95de7da4bbe145d6d574712fcf
Danpl <styoda6@gmail.com>
2026-01-18 00:17:47 +0300

    ороо

--- game_stats_v2/game_stats_v2.cpp
+++ game_stats_v2/game_stats_v2.cpp
@@ -6,44 +6,44 @@ using namespace std;
 
 int main()
 {
-  unsigned int score = 5000;
-  cout << "score: " << score << endl;
+	unsigned int score = 5000;
+	cout << "score: " << score << endl;
 
-  // Изменение значения переменной
-  
-  score = score + 100;
-  cout << "score: " << score << endl;
+	// Изменение значения переменной
 
-  // Комбинированный оператор присваивания
+	score = score + 100;
+	cout << "score: " << score << endl;
 
-  score += 100;
-  cout << "score: " << score << endl;
-  
-  // Использование оператора инкремента ++ увеличивающий значение переменной на единицу
-  // ++lives - сначала переменная увеличивается на 1 потом все остальное
-  // lives++ - присвается сначала потом увеличивается на 1
-  // -- декремент, тоже самое только уменьшает на 1
+	// Комбинированный оператор присваивания
 
-  int lives = 3;
-  ++lives;
-  cout << "lives: " << lives << endl;
+	score += 100;
+	cout << "score: " << score << endl;
 
-  lives = 3;
-  lives++;
-  cout << "lives: " << lives << endl;
+	// Использование оператора инкремента ++ увеличивающий значение переменной на единицу
+	// ++lives - сначала переменная увеличивается на 1 потом все остальное
+	// lives++ - присвается сначала потом увеличивается на 1
+	// -- декремент, тоже самое только уменьшает на 1
 
-  lives = 3;
-  int bonus = ++lives * 10;
-  cout << "lives, bonus: " << lives << ', ' << bonus << endl;
+	int lives = 3;
+	++lives;
+	cout << "lives: " << lives << endl;
 
-  lives = 3;
-  bonus = lives++ * 10;
-  cout << "lives, bonus: " << lives << ', ' << bonus << endl;
+	lives = 3;
+	lives++;
+	cout << "lives: " << lives << endl;
 
-  // Целочисленное переполнение
-  score = 4294967295;
-  cout << "\nscore: " << score << endl;
-  ++score;
-  cout << "score: " << score << endl;
-  return 0; 
+	lives = 3;
+	int bonus = ++lives * 10;
+	cout << "lives, bonus: " << lives << ', ' << bonus << endl;
+
+	lives = 3;
+	bonus = lives++ * 10;
+	cout << "lives, bonus: " << lives << ', ' << bonus << endl;
+
+	// Целочисленное переполнение
+	score = 4294967295;
+	cout << "\nscore: " << score << endl;
+	++score;
+	cout << "score: " << score << endl;
+	return 0;
 }
---
+++ game_stats_v3/game_stats_v3.cpp
@@ -0,0 +1,10 @@
+// game_stats_v3 ασσσσσσσσσσσσσσσσσσσσσσσσσσσβοϋοτοϋοϋοϋοϋ
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
